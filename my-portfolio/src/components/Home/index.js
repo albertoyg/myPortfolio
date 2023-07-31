e5b8230ce1e851620a185f63d2323f67faf695b6
Alberto Yanes <albertoyanesgca@gmail.com>
2023-07-30 22:14:15 -0700

    Homepage edit.
    Name and title animation finished

---
+++ my-portfolio/src/components/Home/index.js
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import AnimatedLetters from "../AnimatedLetters";
+import './index.scss';
+import { useState } from "react";
+
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const name = ['A', 'l', 'b', 'e', 'r', 't', 'o',' ','Y','a','n','e','s','-','G','a','r','c','i','a']
+    const title = ['S','o','f','t','w','a','r','e',' ','D','e','v','e','l','o','p','e','r']
+    
+    return (
+        <div className="container home-page">
+            <div className="text-zone"> 
+                <h1>
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={name}
+                        index={0}/> <br/>
+
+                    <AnimatedLetters 
+                        letterClass={letterClass}
+                        strArray={title}
+                        index={20}/>
+                </h1>
+                    <Link to="/contact" className='flat-button'>CONTACT YA BOY</Link>
+                   
+            </div>
+        </div>
+    )
+
+}
+
+export default Home
--- my-portfolio/src/components/Layout/index.js
+++ my-portfolio/src/components/Layout/index.js
@@ -7,17 +7,7 @@ const Layout = () => {
         <div className="App">
             <Bottombar /> 
             <div className='page'>
-                <span className='tags top-tags'>&lt;body&gt;</span>
-
                 <Outlet />
-
-                <span className='tags bottom-tags'>
-                    &lt;/body&gt;
-                    <br />
-                    <span>
-                        <span className='bottom-tag-html'>&lt;/html&gt;</span>
-                    </span>
-                </span>
             </div>
         </div>
     )
