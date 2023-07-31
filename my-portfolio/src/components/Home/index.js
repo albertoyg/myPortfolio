11b147e425ff10d89f8a76f0d5c87bea965dd06b
Alberto Yanes <albertoyanesgca@gmail.com>
2023-07-31 00:01:55 -0700

    Added bouncing animation on hovering name and title

--- my-portfolio/src/components/Home/index.js
+++ my-portfolio/src/components/Home/index.js
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
 import AnimatedLetters from "../AnimatedLetters";
 import './index.scss';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const name = ['A', 'l', 'b', 'e', 'r', 't', 'o',' ','Y','a','n','e','s','-','G','a','r','c','i','a']
     const title = ['S','o','f','t','w','a','r','e',' ','D','e','v','e','l','o','p','e','r']
     
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+            }, 4000);
+    
+        return () => clearTimeout(timer); 
+    }, []);
+
     return (
         <div className="container home-page">
             <div className="text-zone"> 
