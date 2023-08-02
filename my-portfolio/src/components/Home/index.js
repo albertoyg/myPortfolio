8f04f0f735031e39742574476e0cbb227bc6f872
Alberto Yanes <albertoyanesgca@gmail.com>
2023-08-01 19:36:46 -0700

    Additions to home page
    Map now working correctly. added downloadable resume and other stylings

--- my-portfolio/src/components/Home/index.js
+++ my-portfolio/src/components/Home/index.js
@@ -2,13 +2,33 @@ import { Link } from "react-router-dom";
 import AnimatedLetters from "../AnimatedLetters";
 import './index.scss';
 import { useEffect, useState } from "react";
-import Loader from "react-loaders";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
 
 const Home = () => {
+    // for name and title
     const [letterClass, setLetterClass] = useState('text-animate')
-    const name = ['A', 'l', 'b', 'e', 'r', 't', 'o',' ','Y','a','n','e','s','-','G','a','r','c','i','a']
-    const title = ['S','o','f','t','w','a','r','e',' ','D','e','v','e','l','o','p','e','r']
+    // for map
+    const [isMapVisible, setMapVisibility] = useState(false)
+    const showMap = () => setMapVisibility(true)
+    const hideMap = () => setMapVisibility(false)
+    // for resume
+    const resume = "http://localhost:3000/AlbertoYanesResume.pdf"
+    const downloadResume=(url)=>{
+        const fileName = url.split('/').pop()
+        const aTag = document.createElement('a')
+        aTag.href=url
+        aTag.setAttribute('download',fileName)
+        document.body.appendChild(aTag)
+        aTag.click()
+        aTag.remove()
+    }
     
+    
+
+    
+
+
     useEffect(() => {
         const timer = setTimeout(() => {
             setLetterClass('text-animate-hover');
@@ -18,28 +38,99 @@ const Home = () => {
     }, []);
 
     return (
-        <>
-        
             <div className="container home-page">
+
                 <div className="text-zone"> 
                     <h1>
                         <AnimatedLetters 
                             letterClass={letterClass}
-                            strArray={name}
+                            strArray={['A', 'l', 'b', 'e', 'r', 't', 'o',' ','Y','a','n','e','s','-','G','a','r','c','i','a']}
                             index={0}/> <br/>
 
                         <AnimatedLetters 
                             letterClass={letterClass}
-                            strArray={title}
+                            strArray={['S','o','f','t','w','a','r','e',' ','D','e','v','e','l','o','p','e','r']}
                             index={20}/>
                     </h1>
-                        <Link to="/contact" className='flat-button'>CONTACT YA BOY</Link>
+
+                        <h2>
+
+                            <div className="h2text">
+                                Bachelors of Computer Science <br/>
+                                From the University of Victoria <br/>
+                            </div>
+
+                            <div className="mapButton">
+                                <button onClick={showMap} className="flat-button">Show map</button><br/>
+                            </div>
+                          
+                        </h2>
+                        
+                        <div className="buttonBar">
+
+                            {/* contact button */}
+                            <Link to="/contact" className='flat-button'>
+                                Contact me
+                            </Link>
+
+                            {/* Resume button */}
+                            <button 
+                                className="flat-button"
+                                onClick={() => {
+                                    downloadResume(resume);
+                                }}>
+                                    Download resume
+                                </button>
+
+                            {/* projects button */}
+                            <Link to="/projects" className='flat-button'>
+                                See projects
+                            </Link>
+
+                        </div>
+                        
+
                     
-                    
+
                 </div>
+
+                <div className={`mapBox ${isMapVisible ? 'mapBoxVisible' : ''}`}>
+                    
+                    {isMapVisible && (
+                            <MapContainer 
+                                className="map-container"
+                                center={[48.463254, -123.312164]} 
+                                zoom={15} >
+                                <TileLayer
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                    maxZoom={19} />
+                                <Marker position={[48.463254, -123.312164]}>
+                                    <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+                                </Marker>
+                            </MapContainer>
+                        )}    
+
+                    <div className="hide-map-button" onClick={hideMap}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            viewBox="0 0 20 20"
+                            fill="none"
+                            stroke="#000000"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round">
+                            <path d="M4 4L16 16" />
+                            <path d="M16 4L4 16" />
+                        </svg>
+                    </div>
+
+                </div>
+                    
             </div>
-            <Loader type='pacman' />
-        </>
+
     )
 
 }
